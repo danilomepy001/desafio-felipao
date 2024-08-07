75044577e82800868d897c4c1c8c87a922467942
danilomepy001 <danilo.oliveira.tech@gmail.com>
2024-08-07 14:05:26 -0400

    Primeiro desafio de projeto

---
+++ src/desafio-felipao.js
@@ -0,0 +1,51 @@
+let nomeHeroi = "Jonas"
+
+let xpHeroi = 4000
+
+let nivelHeroi = "Ferro" ; "Bronze" ; "Prata" ; "Ouro" ; "Platina" ; "Ascendente" ; "Imortal" ; "Radiante" 
+
+Ferro = (0<=1000) ;
+Bronze = (1001<=2000) ;
+Prata = (2001<=5000) ;
+Ouro = (5001<=7000) ; 
+Platina = (7001<=8000) ;
+Ascendente = (8001<=9000) ; 
+Imortal = (9001<=10000) ; 
+Radiante = (0<=10001) ;
+
+if (xpHeroi === 0<=1000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível " + nivelHeroi)
+}
+else if (xpHeroi === 1001<=2000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível " + nivelHeroi)
+}
+else if (xpHeroi === 2001<=5001) { 
+    console.log("O herói de nome " + nomeHeroi + " está no nível " + nivelHeroi)
+}
+else if (xpHeroi === 5001<=7000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível " + nivelHeroi)
+}
+else if (xpHeroi === 7001<=8000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível " + nivelHeroi)
+}
+else if (xpHeroi === 8001<=9000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível " + nivelHeroi)
+}
+else if (xpHeroi === 9001<=10000) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível " + nivelHeroi)
+}
+else if (xpHeroi === 0<=10001) {
+    console.log("O herói de nome " + nomeHeroi + " está no nível " + nivelHeroi)
+}
+
+
+
+
+//Se XP for menor do que 1.000 = Ferro
+//Se XP for entre 1.001 e 2.000 = Bronze
+//Se XP for entre 2.001 e 5.000 = Prata
+//Se XP for entre 5.001 e 7.000 = Ouro
+//Se XP for entre 7.001 e 8.000 = Platina
+//Se XP for entre 8.001 e 9.000 = Ascendente
+//Se XP for entre 9.001 e 10.000= Imortal
+//Se XP for maior ou igual a 10.001 = Radiante
